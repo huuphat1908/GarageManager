e375c4d0b4b6296b9de6e76bc68bf8dbfedc9a91
huuphat1908 <huuphat1908@gmail.com>
2022-02-03 11:55:14 +0700

    config netlift.toml

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import store from './redux/store.js'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-axios.defaults.baseURL = 'http://localhost:8080/';
+axios.defaults.baseURL = process.env.API_URL || 'http://localhost:8080/';
 axios.defaults.withCredentials = true
 
 ReactDOM.render(
