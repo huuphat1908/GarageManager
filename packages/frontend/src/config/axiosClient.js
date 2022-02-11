d27d4bd6d970f8f1cd0964ab0c5d171dee1fb890
huuphat1908 <huuphat1908@gmail.com>
2022-02-12 00:58:51 +0700

    modify axios client

--- packages/frontend/src/config/axiosClient.js
+++ packages/frontend/src/config/axiosClient.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const axiosClient = axios.create({
     baseURL: 'https://garage-manager-uit.herokuapp.com/',
+    headers: {
+        'content-type': 'application/json',
+    },
     withCredentials: true
 })
 
